19c855855799108be2ebb7174a10d2522d4d7633
NoCommentsSir <60754793+NoCommentsSir@users.noreply.github.com>
2023-07-12 03:38:02 +0300

    Mobile version

--- source/script.js
+++ source/script.js
@@ -1,8 +1,41 @@
 $(document).ready(function(){
     $(".slider").slick({
-        centerMode: true,
-        centerPadding: '256px',
         dots: true,
         variableWidth: false,
+        responsive: [
+            {
+                breakpoint: 376,
+                settings: {
+                    centerMode: false,
+                    centerPadding: '10px',
+                }
+            },
+            {
+                breakpoint: 1921,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '256px',
+                }
+            },
+        ]
     });
+    $("#action").hover(function () {
+        $(".drop-down-menu").stop(true, false).slideToggle(400);
+        $(".hide-name").stop(true, false).slideToggle(400);
+    });
+    $('#person').bind("change keyup input click", function() { this.value = this.value.replace(/[^а-яёА-ЯЁ\s\-]/g, ''); });
+    $('#number').bind("change keyup input click", function() { this.value = this.value.replace(/[^0-9\+]/g, ''); });
 });
+
+// $(document).ready(function(){
+//     $("#action").hover(
+//     function () {
+//         clearTimeout($.data(this,'timer'));
+//         $('ul',this).stop(true,true).slideDown(200);
+//     },
+//     function () {
+//         $.data(this,'timer', setTimeout($.proxy(function() {
+//         $('ul',this).stop(true,true).slideUp(200);
+//         }, this), 100));
+//     });
+// });
